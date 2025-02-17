13777e4a665885a6e88257549d9ccac4daf87d5d
Magesh Kuppan <tkmagesh77@gmail.com>
2025-02-17 14:59:49 +0530

    assignment - solutions

--- 06-practice/01-problem.go
+++ 06-practice/01-problem.go
@@ -1,3 +1,21 @@
 /*
 Accept a range (start & end) from the user and print all the prime numbers between the given range
 */
+package main
+
+import "fmt"
+
+func main() {
+	var start, end int
+	fmt.Println("Enter the start & end :")
+	fmt.Scanln(&start, &end)
+NO_LOOP:
+	for no := start; no <= end; no++ {
+		for i := 2; i <= (no / 2); i++ {
+			if no%i == 0 {
+				continue NO_LOOP
+			}
+		}
+		fmt.Println("Prime No :", no)
+	}
+}
--- 06-practice/02-problem.go
+++ 06-practice/02-problem.go
@@ -25,3 +25,41 @@ if user choice < 1 OR > 5
 	display the menu again (IMPORTANT)
 
 */
+
+package main
+
+import "fmt"
+
+func main() {
+	var userChoice, x, y, result int
+	for {
+		fmt.Println("1. Add")
+		fmt.Println("2. Subtract")
+		fmt.Println("3. Multiply")
+		fmt.Println("4. Divide")
+		fmt.Println("5. Exit")
+		fmt.Println("Enter your choice :")
+		fmt.Scanln(&userChoice)
+		if userChoice == 5 {
+			fmt.Println("Done!")
+			break
+		}
+		if userChoice < 1 || userChoice > 5 {
+			fmt.Println("Invalid choice")
+			continue
+		}
+		fmt.Println("Enter the operands :")
+		fmt.Scanln(&x, &y)
+		switch userChoice {
+		case 1:
+			result = x + y
+		case 2:
+			result = x - y
+		case 3:
+			result = x * y
+		case 4:
+			result = x / y
+		}
+		fmt.Println("Result :", result)
+	}
+}
