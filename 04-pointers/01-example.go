9c547a70f57d0fd43653ee6d543865344adeb701
Magesh Kuppan <tkmagesh77@gmail.com>
2025-02-17 14:03:08 +0530

    pointers

---
+++ 04-pointers/01-example.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var no int
+	no = 100
+
+	var noPtr *int
+	noPtr = &no
+
+	fmt.Println(noPtr, no)
+
+	// deferencing (pointer -> value)
+	fmt.Println(*noPtr)
+
+	fmt.Println(no == *(&no))
+}
