4cd707962e2cc88a9bf7c958363c42efff51cf0c
Magesh Kuppan <tkmagesh77@gmail.com>
2025-02-18 11:22:22 +0530

    collections - assignment - solution

--- 08-collections/05-assignment.go
+++ 08-collections/05-assignment.go
@@ -14,13 +14,27 @@ func main() {
 	var start, end int
 	fmt.Println("Enter the start & end :")
 	fmt.Scanln(&start, &end)
-NO_LOOP:
-	for no := start; no <= end; no++ {
-		for i := 2; i <= (no / 2); i++ {
-			if no%i == 0 {
-				continue NO_LOOP
-			}
-		}
+	primes := genPrimes(start, end)
+	for _, no := range primes {
 		fmt.Println("Prime No :", no)
 	}
+}
+
+func genPrimes(start, end int) []int {
+	var primes []int
+	for i := start; i <= end; i++ {
+		if isPrime(i) {
+			primes = append(primes, i)
+		}
+	}
+	return primes
+}
+
+func isPrime(no int) bool {
+	for i := 2; i <= (no / 2); i++ {
+		if no%i == 0 {
+			return false
+		}
+	}
+	return true
 }
