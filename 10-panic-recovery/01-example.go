9494e4fa567877b15fc74f9e6ba9acb9a849147f
Magesh Kuppan <tkmagesh77@gmail.com>
2025-02-18 12:23:55 +0530

    recovering from panic

--- 10-panic-recovery/01-example.go
+++ 10-panic-recovery/01-example.go
@@ -4,11 +4,19 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"os"
 )
 
 var ErrZeroDivisor error = errors.New("divisor cannot be 0")
 
 func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("App paniced!, err :", err)
+			os.Exit(2)
+		}
+		fmt.Println("Thank you!")
+	}()
 	divisor := 0
 	q, r := divide(100, divisor)
 	fmt.Println(q, r)
